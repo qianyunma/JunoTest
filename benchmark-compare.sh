e31afb9b767fba5f0da77282f1c37474243a97b6
qianyunma <mazt@ihep.ac.cn>
2016-09-20 18:41:11 +0800

    Update benchmark-compare.sh

--- benchmark-compare.sh
+++ benchmark-compare.sh
@@ -6,9 +6,9 @@ set -o nounset
 
 result_dir=/var/benchmark
 echo -e "Now we will compare two results and see the decline percentage:\n"
-echo -e "Please Input the first file name:\n"
+echo -e "Please Input the first file name:"
 read result_FileName_One
-echo -e "Please Input the second file name:\n"
+echo -e "Please Input the second file name:"
 read result_FileName_Two
 result_path_one=$result_dir/$result_FileName_One
 result_path_two=$result_dir/$result_FileName_Two
@@ -25,75 +25,83 @@ then
     exit 1
 fi
 
+yum -y install jq >/dev/null 2&>1
+
 benchmark_result_one=$(cat ${result_path_one})
 benchmark_result_two=$(cat ${result_path_two})
 
-benchmark_tag="becnchmark result"
-benchmark_score=$"( echo $benchmark_result_one | jq "'."${benchmark_tag}"'" )"
-if [ $benchmark_score -ne 1 ]
+benchmark_tag="benchmarkresult"
+benchmark_score=` echo $benchmark_result_one | jq '.'${benchmark_tag}'' `
+
+tmpconstant="\"1\""
+if [ $benchmark_score != $tmpconstant ]
 then
     echo "Input file "${result_path_one}" is uncorrect!!!"
     exit 1
 fi     
-benchmark_score=$"( echo $benchmark_result_two | jq "'."${benchmark_tag}"'" )"
-if [ $benchmark_score -ne 1 ]
+benchmark_score=` echo $benchmark_result_two | jq '.'${benchmark_tag}'' `
+if [ $benchmark_score != $tmpconstant ]
 then
     echo "Input file "${result_path_two}" is uncorrect!!!"
     exit 1
 fi     
 
 
-unixbench_tag="Unixbench Score"
-One_unixbench_score=$"( echo $benchmark_result_one | jq "'."${unixbench_tag}"'" )"
-Two_unixbench_score=$"( echo $benchmark_result_two | jq "'."${unixbench_tag}"'" )"
+unixbench_tag="UnixbenchScore"
+One_unixbench_score=` echo $benchmark_result_one | jq '.'${unixbench_tag}'' `
+Two_unixbench_score=` echo $benchmark_result_two | jq '.'${unixbench_tag}'' `
 unixbench_DecPercent=$(echo "scale=2; ($One_unixbench_score-$Two_unixbench_score)*100/$One_unixbench_score" | bc -l)"%"
 
 
-Stream_Copy_tag="Stream Copy"
-One_Stream_Copy_score=$"( echo $benchmark_result_one | jq "'."${Stream_Copy_tag}"'" )"
-Two_Stream_Copy_score=$"( echo $benchmark_result_two | jq "'."${Stream_Copy_tag}"'" )"
+Stream_Copy_tag="StreamCopy"
+One_Stream_Copy_score=` echo $benchmark_result_one | jq '.'${Stream_Copy_tag}'' `
+Two_Stream_Copy_score=` echo $benchmark_result_two | jq '.'${Stream_Copy_tag}'' `
 Stream_Copy_DecPercent=$(echo "scale=2; ($One_Stream_Copy_score-$Two_Stream_Copy_score)*100/$One_Stream_Copy_score" | bc -l)"%"
 
-Stream_Scale_tag="Stream SCale"
-One_Stream_Scale_score=$"( echo $benchmark_result_one | jq "'."${Stream_Scale_tag}"'" )"
-Two_Stream_Scale_score=$"( echo $benchmark_result_two | jq "'."${Stream_Scale_tag}"'" )"
+Stream_Scale_tag="StreamSCale"
+One_Stream_Scale_score=` echo $benchmark_result_one | jq '.'${Stream_Scale_tag}'' `
+Two_Stream_Scale_score=` echo $benchmark_result_two | jq '.'${Stream_Scale_tag}'' `
 Stream_Scale_DecPercent=$(echo "scale=2; ($One_Stream_Scale_score-$Two_Stream_Scale_score)*100/$One_Stream_Scale_score" | bc -l)"%"
 
-Stream_Add_tag="Stream Add"
-One_Stream_Add_score=$"( echo $benchmark_result_one | jq "'."${Stream_Add_tag}"'" )"
-Two_Stream_Add_score=$"( echo $benchmark_result_two | jq "'."${Stream_Add_tag}"'" )"
+Stream_Add_tag="StreamAdd"
+One_Stream_Add_score=` echo $benchmark_result_one | jq '.'${Stream_Add_tag}'' `
+Two_Stream_Add_score=` echo $benchmark_result_two | jq '.'${Stream_Add_tag}'' `
 Stream_Add_DecPercent=$(echo "scale=2; ($One_Stream_Add_score-$Two_Stream_Add_score)*100/$One_Stream_Add_score" | bc -l)"%"
 
-Stream_Triad_tag="Stream Triad"
-One_Stream_Triad_score=$"( echo $benchmark_result_one | jq "'."${Stream_Triad_tag}"'" )"
-Two_Stream_Triad_score=$"( echo $benchmark_result_two | jq "'."${Stream_Triad_tag}"'" )"
+Stream_Triad_tag="StreamTriad"
+One_Stream_Triad_score=` echo $benchmark_result_one | jq '.'${Stream_Triad_tag}'' `
+Two_Stream_Triad_score=` echo $benchmark_result_two | jq '.'${Stream_Triad_tag}'' `
 Stream_Triad_DecPercent=$(echo "scale=2; ($One_Stream_Triad_score-$Two_Stream_Triad_score)*100/$One_Stream_Triad_score" | bc -l)"%"
 
-Iozone_Write_tag="Initial Write"
-One_Iozone_Write_score=$"( echo $benchmark_result_one | jq "'."${Iozone_Write_tag}"'" )"
-Two_Iozone_Write_score=$"( echo $benchmark_result_two | jq "'."${Iozone_Write_tag}"'" )"
+Iozone_Write_tag="InitialWrite"
+One_Iozone_Write_score=` echo $benchmark_result_one | jq '.'${Iozone_Write_tag}'' `
+Two_Iozone_Write_score=` echo $benchmark_result_two | jq '.'${Iozone_Write_tag}'' `
 Iozone_Write_DecPercent=$(echo "scale=2; ($One_Iozone_Write_score-$Two_Iozone_Write_score)*100/$One_Iozone_Write_score" | bc -l)"%"
 
 Iozone_ReWrite_tag="Rewrite"
-One_Iozone_ReWrite_score=$"( echo $benchmark_result_one | jq "'."${Iozone_ReWrite_score}"'" )"
-Two_Iozone_ReWrite_score=$"( echo $benchmark_result_two | jq "'."${Iozone_ReWrite_score}"'" )"
+One_Iozone_ReWrite_score=` echo $benchmark_result_one | jq '.'${Iozone_ReWrite_tag}'' `
+Two_Iozone_ReWrite_score=` echo $benchmark_result_two | jq '.'${Iozone_ReWrite_tag}'' `
 Iozone_ReWrite_DecPercent=$(echo "scale=2; ($One_Iozone_ReWrite_score-$Two_Iozone_ReWrite_score)*100/$One_Iozone_ReWrite_score" | bc -l)"%"
 
 Iozone_Read_tag="Read"
-One_Iozone_Read_score=$"( echo $benchmark_result_one | jq "'."${Iozone_Read_score}"'" )"
-Two_Iozone_Read_score=$"( echo $benchmark_result_two | jq "'."${Iozone_Read_score}"'" )"
+One_Iozone_Read_score=` echo $benchmark_result_one | jq '.'${Iozone_Read_tag}'' `
+Two_Iozone_Read_score=` echo $benchmark_result_two | jq '.'${Iozone_Read_tag}'' `
 Iozone_Read_DecPercent=$(echo "scale=2; ($One_Iozone_Read_score-$Two_Iozone_Read_score)*100/$One_Iozone_Read_score" | bc -l)"%"
 
 Iozone_ReRead_tag="ReRead"
-One_Iozone_ReRead_score=$"( echo $benchmark_result_one | jq "'."${Iozone_ReRead_score}"'" )"
-Two_Iozone_ReRead_score=$"( echo $benchmark_result_two | jq "'."${Iozone_ReRead_score}"'" )"
+One_Iozone_ReRead_score=` echo $benchmark_result_one | jq '.'${Iozone_ReRead_tag}'' `
+Two_Iozone_ReRead_score=` echo $benchmark_result_two | jq '.'${Iozone_ReRead_tag}'' `
 Iozone_ReRead_DecPercent=$(echo "scale=2; ($One_Iozone_ReRead_score-$Two_Iozone_ReRead_score)*100/$One_Iozone_ReRead_score" | bc -l)"%"
 
 
 
 
-echo -e "Cpu Test Result:\n\tTest Item:\t"${result_FileName_One}"\t"${result_FileName_Two}"\tDecline Percentage\n\t"${unixbench_tag}":\t"${One_unixbench_score}"\t"${Two_unixbench_score}"\t"${unixbench_DecPercent}"\n"
+echo -e "Cpu Test Result:\n\tTest Item:\t"${result_FileName_One}"\t"${result_FileName_Two}"\tDecline Percentage\n\t"${unixbench_tag}":\t"${One_unixbench_score}"\t\t"${Two_unixbench_score}"\t\t\t"${unixbench_DecPercent}"\n"
 
-echo -e "Memory Test Result:\n\tTest Item:\t"${result_FileName_One}"\t"${result_FileName_Two}"\tDecline Percentage\n\t"${Stream_Copy_tag}":\t"${One_Stream_Copy_score}"\t"${Two_Stream_Copy_score}"\t"${Stream_Copy_DecPercent}"\n\t"${Stream_Scale_tag}":\t"${One_Stream_Scale_score}"\t"${Two_Stream_Scale_score}"\t"${Stream_Scale_DecPercent}"\n\t"${Stream_Add_tag}":\t"${One_Stream_Add_score}"\t"${Two_Stream_Add_score}"\t"${Stream_Add_DecPercent}"\n\t"${Stream_Triad_tag}":\t"${One_Stream_Triad_score}"\t"${Two_Stream_Triad_score}"\t"${Stream_Triad_DecPercent}"\n"
+echo -e "Memory Test Result(s):\n\tTest Item:\t"${result_FileName_One}"\t"${result_FileName_Two}"\tDecline Percentage\n\t"${Stream_Copy_tag}":\t"${One_Stream_Copy_score}"\t\t"${Two_Stream_Copy_score}"\t\t\t"${Stream_Copy_DecPercent}"\n\t"${Stream_Scale_tag}":\t"${One_Stream_Scale_score}"\t\t"${Two_Stream_Scale_score}"\t\t\t"${Stream_Scale_DecPercent}"\n\t"${Stream_Add_tag}":\t"${One_Stream_Add_score}"\t\t"${Two_Stream_Add_score}"\t\t\t"${Stream_Add_DecPercent}"\n\t"${Stream_Triad_tag}":\t"${One_Stream_Triad_score}"\t\t"${Two_Stream_Triad_score}"\t\t\t"${Stream_Triad_DecPercent}"\n"
 
-echo -e "IO Test Speed Result:\n\tTest Item:\t"${result_FileName_One}"\t"${result_FileName_Two}"\tDecline Percentage\n\t"${Iozone_Write_tag}":\t"${One_Iozone_Write_score}"\t"${Two_Stream_Triad_score}"\t"${Iozone_Write_DecPercent}"\n\t"${Iozone_ReWrite_tag}":\t"${One_Iozone_ReWrite_score}"\t"${Two_Iozone_ReWrite_score}"\t"${Iozone_ReWrite_DecPercent}"\n\t"${Iozone_Read_tag}":\t"${One_Iozone_Read_score}"\t"${Two_Iozone_Read_score}"\t"${Iozone_Read_DecPercent}"\n\t"${Iozone_ReRead_tag}":\t"${One_Iozone_ReRead_score}""${Two_Iozone_ReRead_score}""${Iozone_ReRead_DecPercent}"\n"
+echo -e "IO Test Speed Result(MB/S):\n\tTest Item:\t"${result_FileName_One}"\t"${result_FileName_Two}"\tDecline Percentage\n\t"${Iozone_Write_tag}":\t"${One_Iozone_Write_score}"\t\t"${Two_Stream_Triad_score}"\t\t\t"${Iozone_Write_DecPercent}"\n\t"${Iozone_ReWrite_tag}":\t"${One_Iozone_ReWrite_score}"\t\t"${Two_Iozone_ReWrite_score}"\t\t\t"${Iozone_ReWrite_DecPercent}"\n\t"${Iozone_Read_tag}":\t\t"${One_Iozone_Read_score}"\t\t"${Two_Iozone_Read_score}"\t\t\t"${Iozone_Read_DecPercent}"\n\t"${Iozone_ReRead_tag}":\t\t"${One_Iozone_ReRead_score}"\t\t"${Two_Iozone_ReRead_score}"\t\t\t"${Iozone_ReRead_DecPercent}"\n"
+
+
+
+
