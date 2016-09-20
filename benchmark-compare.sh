1182b3433a02aa9819caf0ae8172edd0ab89843d
qianyunma <mazt@ihep.ac.cn>
2016-09-20 23:43:17 +0800

    Update benchmark-compare.sh

--- benchmark-compare.sh
+++ benchmark-compare.sh
@@ -53,7 +53,7 @@ One_unixbench_score=`echo $One_unixbench_score | sed 's/\"//g'`
 Two_unixbench_score=` echo $benchmark_result_two | jq '.'${unixbench_tag}'' `
 Two_unixbench_score=`echo $Two_unixbench_score | sed 's/\"//g'`
 unixbench_DecPercent=$(echo "scale=2; ($One_unixbench_score-$Two_unixbench_score)*100/$One_unixbench_score" | bc -l)"%"
-
+unixbench_DecPercent=` echo $unixbench_DecPercent | sed 's/^\./0./' `
 
 Stream_Copy_tag="StreamCopy"
 One_Stream_Copy_score=` echo $benchmark_result_one | jq '.'${Stream_Copy_tag}'' `
@@ -61,6 +61,7 @@ One_Stream_Copy_score=`echo $One_Stream_Copy_score | sed 's/\"//g'`
 Two_Stream_Copy_score=` echo $benchmark_result_two | jq '.'${Stream_Copy_tag}'' `
 Two_Stream_Copy_score=`echo $Two_Stream_Copy_score | sed 's/\"//g'`
 Stream_Copy_DecPercent=$(echo "scale=2; ($One_Stream_Copy_score-$Two_Stream_Copy_score)*100/$One_Stream_Copy_score" | bc -l)"%"
+Stream_Copy_DecPercent=` echo $Stream_Copy_DecPercent | sed 's/^\./0./' `
 
 Stream_Scale_tag="StreamSCale"
 One_Stream_Scale_score=` echo $benchmark_result_one | jq '.'${Stream_Scale_tag}'' `
@@ -68,6 +69,7 @@ One_Stream_Scale_score=`echo $One_Stream_Scale_score | sed 's/\"//g'`
 Two_Stream_Scale_score=` echo $benchmark_result_two | jq '.'${Stream_Scale_tag}'' `
 Two_Stream_Scale_score=`echo $Two_Stream_Scale_score | sed 's/\"//g'`
 Stream_Scale_DecPercent=$(echo "scale=2; ($One_Stream_Scale_score-$Two_Stream_Scale_score)*100/$One_Stream_Scale_score" | bc -l)"%"
+Stream_Scale_DecPercent=` echo $Stream_Scale_DecPercent | sed 's/^\./0./' `
 
 Stream_Add_tag="StreamAdd"
 One_Stream_Add_score=` echo $benchmark_result_one | jq '.'${Stream_Add_tag}'' `
@@ -75,6 +77,7 @@ One_Stream_Add_score=`echo $One_Stream_Add_score | sed 's/\"//g'`
 Two_Stream_Add_score=` echo $benchmark_result_two | jq '.'${Stream_Add_tag}'' `
 Two_Stream_Add_score=`echo $Two_Stream_Add_score | sed 's/\"//g'`
 Stream_Add_DecPercent=$(echo "scale=2; ($One_Stream_Add_score-$Two_Stream_Add_score)*100/$One_Stream_Add_score" | bc -l)"%"
+Stream_Add_DecPercent=` echo $Stream_Add_DecPercent | sed 's/^\./0./' `
 
 Stream_Triad_tag="StreamTriad"
 One_Stream_Triad_score=` echo $benchmark_result_one | jq '.'${Stream_Triad_tag}'' `
@@ -82,6 +85,7 @@ One_Stream_Triad_score=`echo $One_Stream_Triad_score | sed 's/\"//g'`
 Two_Stream_Triad_score=` echo $benchmark_result_two | jq '.'${Stream_Triad_tag}'' `
 Two_Stream_Triad_score=`echo $Two_Stream_Triad_score | sed 's/\"//g'`
 Stream_Triad_DecPercent=$(echo "scale=2; ($One_Stream_Triad_score-$Two_Stream_Triad_score)*100/$One_Stream_Triad_score" | bc -l)"%"
+Stream_Triad_DecPercent=` echo $Stream_Triad_DecPercent | sed 's/^\./0./' `
 
 Iozone_Write_tag="InitialWrite"
 One_Iozone_Write_score=` echo $benchmark_result_one | jq '.'${Iozone_Write_tag}'' `
@@ -89,6 +93,7 @@ One_Iozone_Write_score=`echo $One_Iozone_Write_score | sed 's/\"//g'`
 Two_Iozone_Write_score=` echo $benchmark_result_two | jq '.'${Iozone_Write_tag}'' `
 Two_Iozone_Write_score=`echo $Two_Iozone_Write_score | sed 's/\"//g'`
 Iozone_Write_DecPercent=$(echo "scale=2; ($One_Iozone_Write_score-$Two_Iozone_Write_score)*100/$One_Iozone_Write_score" | bc -l)"%"
+Iozone_Write_DecPercent=` echo $Iozone_Write_DecPercent | sed 's/^\./0./' `
 
 Iozone_ReWrite_tag="Rewrite"
 One_Iozone_ReWrite_score=` echo $benchmark_result_one | jq '.'${Iozone_ReWrite_tag}'' `
@@ -96,6 +101,7 @@ One_Iozone_ReWrite_score=`echo $One_Iozone_ReWrite_score | sed 's/\"//g'`
 Two_Iozone_ReWrite_score=` echo $benchmark_result_two | jq '.'${Iozone_ReWrite_tag}'' `
 Two_Iozone_ReWrite_score=`echo $Two_Iozone_ReWrite_score | sed 's/\"//g'`
 Iozone_ReWrite_DecPercent=$(echo "scale=2; ($One_Iozone_ReWrite_score-$Two_Iozone_ReWrite_score)*100/$One_Iozone_ReWrite_score" | bc -l)"%"
+Iozone_ReWrite_DecPercent=` echo $Iozone_ReWrite_DecPercent | sed 's/^\./0./' `
 
 Iozone_Read_tag="Read"
 One_Iozone_Read_score=` echo $benchmark_result_one | jq '.'${Iozone_Read_tag}'' `
@@ -103,6 +109,7 @@ One_Iozone_Read_score=`echo $One_Iozone_Read_score | sed 's/\"//g'`
 Two_Iozone_Read_score=` echo $benchmark_result_two | jq '.'${Iozone_Read_tag}'' `
 Two_Iozone_Read_score=`echo $Two_Iozone_Read_score | sed 's/\"//g'`
 Iozone_Read_DecPercent=$(echo "scale=2; ($One_Iozone_Read_score-$Two_Iozone_Read_score)*100/$One_Iozone_Read_score" | bc -l)"%"
+Iozone_Read_DecPercent=` echo $Iozone_Read_DecPercent | sed 's/^\./0./' `
 
 Iozone_ReRead_tag="ReRead"
 One_Iozone_ReRead_score=` echo $benchmark_result_one | jq '.'${Iozone_ReRead_tag}'' `
@@ -110,6 +117,7 @@ One_Iozone_ReRead_score=`echo $One_Iozone_ReRead_score | sed 's/\"//g'`
 Two_Iozone_ReRead_score=` echo $benchmark_result_two | jq '.'${Iozone_ReRead_tag}'' `
 Two_Iozone_ReRead_score=`echo $Two_Iozone_ReRead_score | sed 's/\"//g'`
 Iozone_ReRead_DecPercent=$(echo "scale=2; ($One_Iozone_ReRead_score-$Two_Iozone_ReRead_score)*100/$One_Iozone_ReRead_score" | bc -l)"%"
+Iozone_ReRead_DecPercent=` echo $Iozone_ReRead_DecPercent | sed 's/^\./0./' `
 
 
 
